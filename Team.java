e3475b9ef538684a370514eccbb3526dc3208749
Jacob Agee <98362450+jacobagee21@users.noreply.github.com>
2024-03-22 22:59:05 -0400

    Add files via upload

---
+++ Team.java
@@ -0,0 +1,53 @@
+public class Team {
+
+    private String school;
+    private String sport;
+    private String mascot;
+
+    public final static String MOTTO = "Sportsmanship!";
+
+    public Team() {
+
+
+    }//ends team construct
+
+    public Team(String aSchool, String aSport, String aMascot) {
+
+        school = aSchool;
+        sport = aSport;
+        mascot = aMascot;
+
+
+    }//ends Team class
+
+
+
+    //get for school
+    public String getSchool(){
+
+return school;
+    }
+    public String getSport(){
+
+        return sport;
+    }
+    public String getMascot(){
+
+        return mascot;
+    }
+
+
+
+    //set the getters ands setters
+
+
+
+
+
+
+
+
+
+
+
+}
