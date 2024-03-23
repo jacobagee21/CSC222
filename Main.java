51f6ae41987ec180d5fcac627288c206554bb0dc
Jacob Agee <98362450+jacobagee21@users.noreply.github.com>
2024-03-22 23:03:31 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,6 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+// test
+
+
+        Team team1 = new Team("PCHS", "Basketball", "Cougars");
+
+        System.out.println("team 1: " + team1.getSchool());
+
+
+
+
 
         }
     }
